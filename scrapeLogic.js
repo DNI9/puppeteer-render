82ed63d1e333f5520bb004b438595462e7f0c95f
DNI9 <itsr9boi@gmail.com>
2024-06-02 01:49:04 +0530

    upgrade deps

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 require("dotenv").config();
 
-const scrapeLogic = async (res) => {
+const scrapeLogic = async res => {
   const browser = await puppeteer.launch({
     args: [
       "--disable-setuid-sandbox",
@@ -22,19 +22,7 @@ const scrapeLogic = async (res) => {
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
 
-    // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
-
-    // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
-
-    // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
+    const fullTitle = await page.title();
 
     // Print the full title
     const logStatement = `The title of this blog post is ${fullTitle}`;
